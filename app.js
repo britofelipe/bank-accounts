315ad639e62bd5328cebaebbe08e76c556cd91ef
arrudabritofelipe@gmail.com <felipe.brito@assembleia.local>
2023-01-17 16:20:55 -0300

    Deposit operation

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ function operation() {
         .then((answer) => {
             const action = answer["action"]
             
-            if(action === "Create account") createAccount();
+            if(action === "Create account") 
+                createAccount();
+            else if(action === "Check balance")
+                checkBalance();
+            else if(action === "Deposit")
+                deposit();
+            else if(action === "Withdraw")
+                checkBalance();
+            else if(action === "Exit")
+                exit();
         })
         .catch((err) => console.log(err))
 }
@@ -41,6 +50,7 @@ function createAccount() {
     configureAccount();
 }
 
+// Add account name
 function configureAccount() {
     inquirer
         .prompt([
@@ -49,29 +59,107 @@ function configureAccount() {
                 message: "Write your account name:"
             }
         ])
-    .then((answer) => {
-        const accountName = answer["accountName"] 
-        console.info(accountName)
+        .then((answer) => {
+            const accountName = answer["accountName"] 
+            console.info(accountName)
 
-        if(!fs.existsSync("accounts")) {
-            fs.mkdirSync("accounts")
-        }
-        if(fs.existsSync(`accounts/${accountName}.json`)) {
-            console.log(chalk.bgRed("This account already exists. Please choose another name"))
-            configureAccount()
-        }
-        fs.writeFileSync(
-            `accounts/${accountName}.json`, 
-            '{"balance": 0}',
-            function (err) {
-                console.log(err)
+            if(!fs.existsSync("accounts")) {
+                fs.mkdirSync("accounts")
             }
-        )
-        console.log(chalk.bgGreen("Congrats! Your account was just created and it is ready to use!"))
-        operation()
-    })
-    .catch((err) => console.log(err))
+            if(fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(chalk.bgRed("This account already exists. Please choose another name"))
+                configureAccount()
+            }
+            fs.writeFileSync(
+                `accounts/${accountName}.json`, 
+                '{"balance": 0}',
+                function (err) {
+                    console.log(err)
+                }
+            )
+            console.log(chalk.bgGreen("Congrats! Your account was just created and it is ready to use!"))
+            operation()
+         })
+        .catch((err) => console.log(err))
 }
 
+// Check if account exists
+function checkAccount(accountName) {
+    if(!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black("This account doesn't exist. Please choose a valid account"))
+        return false
+    }
+    return true
+}
+
+// CHECK BALANCE OF THE ACCOUNT
+function checkBalance() {
+
+}
+
+// MAKE A DEPOSIT
+function deposit() {
+    inquirer.prompt([
+        {
+            name: "accountName",
+            message: "What is your account's name?"
+        }
+    ])
+    .then((answer)=> {
+        accountName = answer["accountName"]
+        // verify if account exists
+        if(!checkAccount(accountName)) {
+            return deposit()
+        }
+        inquirer.prompt([
+            {
+                name: "amount",
+                message: "How much do you want to deposit?"
+            }
+        ]).then((answer) => {
+            const amount = answer["amount"]
+
+            // add an amount
+            addAmount(accountName, amount)
+            operation()
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+}
+
+function addAmount(accountName, amount) {
+    const accountData = getAccountByName(accountName)
+    if(!amount) {
+        console.log(
+            chalk.bgRed.black("Please provide a valid amount")
+        )
+        return deposit()
+    }
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (err) {
+            console.log(err)
+        },
+    )
+    console.log(chalk.green(`The amount of ${amount} has been successfully deposited`))
+}
+
+function getAccountByName(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: "utf8",
+        flag: "r"
+    })
+    
+    return JSON.parse(accountJSON)
+}
+
+// EXIT THE PROGRAM
+function exit() {
+    console.log(chalk.bgCyan.black("Thank you for using our services. See you later!"))
+    process.exit()
+}
 
 // https://www.npmjs.com/package/inquirer#documentation
